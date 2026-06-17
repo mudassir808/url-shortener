62eafcd343112a223a926ff019884732d1baff53
Mudassir Ahmad <ahmadmudassir808@gmail.com>
2026-06-17 23:12:29 +0530

    Initial Spring Boot setup

---
+++ src/test/java/com/mudassir/urlshortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mudassir.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
